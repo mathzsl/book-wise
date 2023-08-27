80ab0da69bd2871bd843f5f6197a129758c7a94e
mathzsl <maths.soares99@gmail.com>
2023-08-27 03:46:58 -0300

    feat: auth with google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GitHubProvider, { GithubProfile } from "next-auth/providers/github";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
 export function buildNextNextAuthOptions(
   req: NextApiRequest,
@@ -21,6 +22,19 @@ export function buildNextNextAuthOptions(
             name: profile.name!,
             email: profile.email!,
             avatar_url: profile.avatar_url,
+          };
+        },
+      }),
+
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+            avatar_url: profile.picture,
           };
         },
       }),
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
 import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export default function Home() {
   const { data } = useSession();
 
   return (
-    <h1 className="bg-gradient-horizontal h-40">
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </h1>
+    <pre>
+      {JSON.stringify(data, null, 2)}
+      <button onClick={() => signOut()}>Deslogar</button>
+    </pre>
   );
 }
